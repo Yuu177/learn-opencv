27573e81d8c7541c62fc65ebfe55bad2e64a720d
Yuu177 <1322129148@qq.com>
2023-11-11 00:32:41 +0800

    Update face-detection code

--- face-detection/main.cpp
+++ face-detection/main.cpp
@@ -13,14 +13,14 @@ int main() {
   // Load the model
   cv::dnn::Net caffe_net = cv::dnn::readNetFromCaffe(model_config, model_binary);
   if (caffe_net.empty()) {
-    std::cerr << "load caffe model failed" << std::endl;
+    std::cerr << "Load caffe model failed" << std::endl;
     exit(-1);
   }
 
   // Read an image
   cv::Mat frame = cv::imread("image/face.jpg");
   if (frame.empty()) {
-    std::cerr << "load image failed" << std::endl;
+    std::cerr << "Load image failed" << std::endl;
     exit(-1);
   }
 
@@ -30,18 +30,27 @@ int main() {
 
   // 模型推理
   cv::Mat detection = caffe_net.forward("detection_out");
-  // blob(detection) 输出为 [1, 1, x, 7]，这是一个四维数组
-  // 其中 x 是最后保留的框（检测到人脸）的个数，最后一维存放每次检测结果的数据，不同的人脸模型的输出可能是不一样的
-  // 最后一维每一列的含义：
-  // 列 0：物体存在的置信度 TODO
-  // 列 1：包围盒的置信度 TODO
-  // 列 2：检测到的人脸置信度
-  // 列 3：人脸边框左上角坐标 x 在整张图片中的比例
-  // 列 4：人脸边框左上角坐标 y 在整张图片中的比例
-  // 列 5：人脸边框右下角坐标 x 在整张图片中的比例
-  // 列 6：人脸边框右下角坐标 y 在整张图片中的比例
 
-  // 所以 detection.size[2] 是检测到的对象的数量 TODO 有误，detection.size[3] 是每次检测的结果的数据（人脸边界框数据和置信度）
+  int dims = detection.dims;
+  // 输出维度数
+  std::cout << "Number of dimensions: " << dims << std::endl;
+  // 输出每个维度的大小
+  for (int i = 0; i < dims; ++i) {
+      std::cout << detection.size[i] << " ";
+  }
+  std::cout << std::endl;
+  // blob(detection) 输出为 [1, 1, x, 7]，这是一个四维数组
+  // 其中 x 是最后保留的框的个数（这里模型的输出有 200，但不一定都有人脸），最后一维存放每次检测结果的数据。不同的人脸模型的输出可能是不一样的
+  // 最后一维每一列的含义：
+  // 列 0：物体存在的置信度
+  // 列 1：包围盒的置信度
+  // 列 2：检测到的人脸置信度
+  // 列 3：人脸边界框的左上角坐标 x 在整张图片中的比例
+  // 列 4：人脸边界框的左上角坐标 y 在整张图片中的比例
+  // 列 5：人脸边界框的右下角坐标 x 在整张图片中的比例
+  // 列 6：人脸边界框的右下角坐标 y 在整张图片中的比例
+
+  // 所以 detection.size[2] 是检测到的对象的数量，detection.size[3] 是每次检测的结果的数据（人脸边界框数据和置信度）
   // 类似于：
   // std::array<float, 7> FaceInfo;
   // vector<FaceInfo> face;
@@ -51,15 +60,12 @@ int main() {
   cv::Mat detection_mat(detection.size[2], detection.size[3], CV_32F, detection.ptr<float>()); // 转换成了二维数组，方便运算
 
   // Draw the rectangles
-  // std::cout << detection_mat.rows << std::endl;
-  std::cout << detection.size[2] << std::endl;
   for (int i = 0; i < detection_mat.rows; i++) {
-    // std::cout << detection_mat.at<float>(i, 0) << ", " << detection_mat.at<float>(i, 1) << std::endl;
-
     float confidence = detection_mat.at<float>(i, 2);
     // 置信度（confidence score）是指模型对于特定的输入数据的预测结果的确信度，
     // 通常以 0~1 的值表示。一般来说，如果模型的置信度越高，它的预测结果就越可信。
-    if (confidence > 0.5) {
+    // 这里一共有 200 个检测到的人脸边界框，我们要选择置信度高的人脸边界框
+    if (confidence > 0.7) {
       auto x_top_left = detection_mat.at<float>(i, 3) * static_cast<float>(frame.cols);
       auto y_top_left = detection_mat.at<float>(i, 4) * static_cast<float>(frame.rows);
       auto x_bottom_right = detection_mat.at<float>(i, 5) * static_cast<float>(frame.cols);
