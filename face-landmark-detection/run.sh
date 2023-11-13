449148137946e99a020a078e0cc5873633908d52
panyu <tanpanyu@minieye.cc>
2023-11-13 11:37:12 +0800

    Rename to face-landmark-detection

--- face-landmark-detection/run.sh
+++ face-landmark-detection/run.sh
@@ -4,4 +4,4 @@ set -x
 
 # LD_LIBRARY_PATH 告诉 loader 在哪些目录中可以找到共享库。可以设置多个搜索目录，这些目录之间用冒号分隔开
 export LD_LIBRARY_PATH=../build/runtime:${LD_LIBRARY_PATH}
-../build/bin/facial-landmarks
+../build/bin/face-landmark-detection
